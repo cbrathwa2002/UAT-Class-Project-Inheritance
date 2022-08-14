0e729766f2125fa6d176837961762acba629fde9
Clarence Brathwaite <Clarence.Brathwaite@ey.com>
2022-08-14 10:00:34 -0400

    person

---
+++ UAT-Class-Project-Inhertance/PersonGroupMember.cs
@@ -0,0 +1,18 @@
+﻿namespace exceltorixml
+{
+    public class PersonGroupMember
+    {
+        public string primaryIndicator { get; set; }
+        public int sequence { get; set; }
+        public string personID { get; set; }
+        public string FamilyName { get; set; }
+        public string GivenName { get; set; }
+        public string DisplayName { get; set; }
+        public string JobTitle { get; set; }
+        public string ContactInfo { get; set; }
+        public string nature { get; set; }
+        public string Email { get; set; }
+        public string personLabel { get; set; }     
+        public string personLabelContext { get; set; }       
+    }
+}
